5cebd2850e6c94341a5d0c1b3a0ec587c400496a
hofbrand <hofbrand.yauheni@gmail.com>
2021-01-18 19:34:40 +0300

    level select
    added level select

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public SceneFader sceneFader;
+
+    public void Select(string levelName)
+    {
+        sceneFader.FadeTo(levelName);
+    }
+}
